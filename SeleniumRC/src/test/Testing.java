b11c2bc61c22ac7c202a969ec54062b9d8806edb
SDarji <SDarji@INHYDLSDARJI.logisticsdev.intra>
2020-07-07 22:50:57 +0530

    commit

---
+++ SeleniumRC/src/test/Testing.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		System.out.println("Wellcome!!!");
+		
+		System.out.println("Wellcome!!!");
+	}
+	
+	
+
+	
+}
